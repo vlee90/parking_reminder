9ae9d7f30fb9d9b3e3d2c26b5bb6e95bee80979d
Vincent Lee <lee.m.vincent@gmail.com>
2018-02-19 14:23:05 -0800

    Initial Commit

---
+++ parking_reminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parking_reminder
+//
+//  Created by Vincent Lee on 2/19/18.
+//  Copyright © 2018 Vincent Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
