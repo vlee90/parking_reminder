6db7ab4427877819c07fd4b4881dab54095b9350
Vincent Lee <lee.m.vincent@gmail.com>
2018-02-19 15:10:43 -0800

    Added Map, Setting, and Tabbar ViewControllers.

---
+++ parking_reminder/SettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingViewController.swift
+//  parking_reminder
+//
+//  Created by Vincent Lee on 2/19/18.
+//  Copyright © 2018 Vincent Lee. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
