e2ccda5b8b9bbc155517c63aaa5c356be76d27d9
Vincent Lee <lee.m.vincent@gmail.com>
2018-02-20 19:49:44 -0800

    Added two tableViews to SettingViewController

--- parking_reminder/ViewControllers/SettingViewController.swift
+++ parking_reminder/ViewControllers/SettingViewController.swift
@@ -9,27 +9,34 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-
+    @IBOutlet weak var topTableView: UITableView!
+    @IBOutlet weak var bottonTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blue
-        // Do any additional setup after loading the view.
+        self.topTableView.tag = 0
+        self.bottonTableView.tag = 1
+        self.topTableView.dataSource = self
+        self.bottonTableView.dataSource = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension SettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (tableView.tag == 0) {
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+            cell.backgroundColor = UIColor.green
+            return cell
+        }
+        else {
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+            cell.backgroundColor = UIColor.blue
+            return cell
+        }
     }
-    */
-
 }
